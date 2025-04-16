86a9687d93be504113585b91ccabcefcf9672d18
Muacidevjava <muaci@yahoo.com>
2025-04-16 07:40:44 -0300

    inicio do projeto

---
+++ agenda/agenda/app/controllers/ClienteController.php
@@ -0,0 +1,35 @@
+<?php
+namespace app\controllers;
+
+use app\core\Controller;
+use app\models\service\Service;
+use app\core\Flash;
+use app\models\service\ClienteService;
+
+class ClienteController extends Controller{
+
+    private $tabela = "cliente";
+    private $campo = "id_cliente";
+   
+    public function index(){
+       $dados["view"]  = "Cliente/Index";
+       $this->load("template", $dados);
+    }
+    
+    public function create(){
+        $dados["view"] = "Cliente/Create";
+        $this->load("template", $dados);
+    }
+    
+    public function edit($id){
+        $dados["view"]      = "Cliente/Create";
+        $this->load("template", $dados);
+    }
+    
+    public function salvar(){
+    }
+    
+    public function excluir($id){
+    }
+}
+
