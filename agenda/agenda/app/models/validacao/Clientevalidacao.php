b6e0e5f4781e47c26cdda809d8fcf91716a39de6
Muacidevjava <muaci@yahoo.com>
2025-04-16 09:29:03 -0300

    criando a classe validacao

---
+++ agenda/agenda/app/models/validacao/Clientevalidacao.php
@@ -0,0 +1,28 @@
+<?php
+namespace app\models\validacao;
+use app\core\Validacao;
+
+class Clientevalidacao {
+      public static function salvar($cliente){
+
+        $validacao = new Validacao();
+                    
+        $validacao->setData('cliente', $cliente->cliente);
+        $validacao->setData('cpf', $cliente->cpf);
+        $validacao->setData('cep', $cliente->cep);
+        $validacao->setData('bairro', $cliente->bairro);
+        $validacao->setData('cidade', $cliente->cidade);
+        $validacao->setData('uf',$cliente->uf);
+
+        
+        $validacao->getData('cep')->isVazio();
+        $validacao->getData('bairro')->isVazio();
+        $validacao->getData('cidade') ->isVazio();
+        $validacao->getData('uf')->isVazio();
+
+        return $validacao;
+
+
+      }
+
+}
